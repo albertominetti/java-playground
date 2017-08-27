e7ca9c625753320dc4801bc4d43869a75af43b71
Alberto Minetti <alberto.minetti@rumbo.com>
2017-08-27 18:32:24 +0200

    using Arrays.binarySearch

---
+++ src/main/java/oca/SearchInArrays.java
@@ -0,0 +1,23 @@
+package oca;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class SearchInArrays {
+  public static void main(String[] args) {
+
+
+    int[] array = {10,20,30,40,50};
+    Arrays.sort(array);
+
+    int i = Arrays.binarySearch(array, 30); // found at 2
+    System.out.println("found at position " + i);
+
+    i = Arrays.binarySearch(array, 32);// should be in position 3, so negate it and minus 1: -4
+    System.out.println("not found, result: " + i);
+
+
+  }
+}
